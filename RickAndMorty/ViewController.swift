af1a1fc846955fb0a55f3925715a91b958028545
Leticia <leticia.echarri@gmail.com>
2024-04-28 19:52:59 +0200

    Create project

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Leticia Echarri on 28/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
